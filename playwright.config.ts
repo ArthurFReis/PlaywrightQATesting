6572dfb3841f9d7d2d588447f8701bb0a3293653
ArthurFReis <arfure21@gmail.com>
2026-05-05 09:07:19 -0300

    validacao dos produtos

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,7 +14,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
  
   retries: 1,
-  reporter: [['json', 'html'], ['junit', {outputFile: 'results.xml'}]],
+  reporter: [['html'], ['junit', {outputFile: 'results.xml'}]],
   
   globalTimeout: 600,
   fullyParallel: true,
--- tests/FrontEnd/products.spec.ts
+++ tests/FrontEnd/products.spec.ts
@@ -35,13 +35,102 @@ test.describe.parallel('Products', () => {
     test('products adicionar produto no carrinho', async ({ page }) => {
         const navigationPage = new NavegationPage(page);
         await navigationPage.productsPage();
-        const adicionar = await page.locator('[data-id="2"]').isDisabled();
-        if (adicionar === true) {
-            console.log('O botão de adicionar ao carrinho está desabilitado, verifique os campos de preenchimento');
+
+        let idProduto = [];
+        let nomeProduto = [];
+        let precoProduto = [];
+        let id = 0;
+        let ids = [1, 2, 3, 4, 5];
+        let cartCount = 0;
+
+
+
+        for (let i = 0; i < ids.length; i++) {
+            id = ids[i];
+
+            if (id === 1){
+                let statusbotao = await page.locator(`[data-id="${id}"]`).isDisabled();
+                if (statusbotao === true) {
+                    console.log('O botão de adicionar ao carrinho está desabilitado, verifique os campos de preenchimento');
+                }
+            }
+            else if (id === 2){
+                let statusbotao = await page.locator(`[data-id="${id}"]`).isDisabled();
+                if (statusbotao === true) {
+                    console.log('O botão de adicionar ao carrinho está desabilitado, verifique os campos de preenchimento');
+                }
+                
+            }
+            else if (id === 3){
+                let statusbotao = await page.locator(`[data-id="${id}"]`).isDisabled();
+                if (statusbotao === true) {
+                    console.log('O botão de adicionar ao carrinho está desabilitado, verifique os campos de preenchimento');
+                }
+            }
+            else if (id === 4){
+                let statusbotao = await page.locator(`[data-id="${id}"]`).isDisabled();
+                if (statusbotao === true) {
+                    console.log('O botão de adicionar ao carrinho está desabilitado, verifique os campos de preenchimento');
+                }
+            }
+            else if (id === 5){
+                let statusbotao = await page.locator(`[data-id="${id}"]`).isDisabled();
+                if (statusbotao === true) {
+                    console.log('O botão de adicionar ao carrinho está desabilitado, verifique os campos de preenchimento');
+                }
+            }
+
+            switch (id) {
+                case 1:
+                    await page.click(`[data-id="${id}"]`);
+                    idProduto.push(id);
+                    nomeProduto.push('Keyboard');
+                    precoProduto.push(120.90);
+                    cartCount = 1;
+                    break;
+
+                case 2:
+                    await page.click(`[data-id="${id}"]`);
+                    idProduto.push(id);
+                    nomeProduto.push('Mouse');
+                    precoProduto.push(79.50);
+                    cartCount = 1;    
+                    break;
+
+                case 3:
+                    await page.click(`[data-id="${id}"]`);
+                    idProduto.push(id);
+                    nomeProduto.push('Monitor');
+                    precoProduto.push(1299.00);
+                    break; 
+
+                case 4:
+                    await page.click(`[data-id="${id}"]`);
+                    idProduto.push(id);
+                    nomeProduto.push('Headset');
+                    precoProduto.push(240.00);
+                    cartCount = 1;
+                    break;
+
+                case 5:
+                    await page.click(`[data-id="${id}"]`);
+                    idProduto.push(id);
+                    nomeProduto.push('Webcam');
+                    precoProduto.push(320.00);
+                    cartCount = 1;
+                    break;
+
+                default:
+                    console.log('Id do produto não encontrado, verifique o id do produto');
+            }
         }
-        else {
-            await page.click('[data-id="2"]');
+
+        console.log('Todos os botões estão ativados \n');
+
+        for (let i = 0; i < idProduto.length; i++) {
+            console.log(`Produto adicionado ao carrinho: ID: ${idProduto[i]}, Nome: ${nomeProduto[i]}, Preço: R$${precoProduto[i]}, Quantidade: ${cartCount}`);
         }
+        console.log('\n O valor do cart é:', await page.locator('#cart-count').textContent());
         await page.screenshot({path: "Evidencias/Products/AdicionarProdutoCart.png"});
     });
 
