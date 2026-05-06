90a2f51d76a9cb761a87dc65af5f28f65d3b5f82
ArthurFReis <arfure21@gmail.com>
2026-05-06 17:09:57 -0300

    evidencias de tamanhos de home

--- tests/FrontEnd/home.spec.ts
+++ tests/FrontEnd/home.spec.ts
@@ -19,7 +19,8 @@ test.describe.parallel('Login', () => {
       await page.setViewportSize({ width: tamanhos.width[tamanho], height: tamanhos.height[tamanho]});
       await page.goto('http://localhost:8080/');
       await expect(page).toHaveURL('http://localhost:8080/');
-      await page.screenshot({path: "Evidencias/Home/home-mobile.png.png"});
+      await page.screenshot({path: `Evidencias/Home/home-mobile${tamanhos.width[tamanho]}.png`});
+    
   }
   console.log("É responsivo! \n");
 });
